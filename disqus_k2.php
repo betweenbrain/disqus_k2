5f1a3cfd767a512edabd4f2ce80147ec791cf35b
Matt Thomas <matt@betweenbrain.com>
2013-10-01 11:06:32 -0400

    Sets disqus_title based on page title

--- disqus_k2.php
+++ disqus_k2.php
@@ -18,10 +18,10 @@ class plgK2Disqus_K2 extends K2Plugin {
 	var $pluginNameHumanReadable = 'DISQUS for K2 plugin';
 
 	function plgK2Disqus_K2( & $subject, $params) {
-	parent::__construct($subject, $params); 
+	parent::__construct($subject, $params);
 	jimport( 'joomla.html.parameter' );
 	$this->plugin = &JPluginHelper::getPlugin('k2', 'disqus_k2');
-	$this->params = new JParameter($this->plugin->params);	
+	$this->params = new JParameter($this->plugin->params);
 	}
 
 	function onK2BeforeDisplay ( &$item, &$params, $limitstart){
@@ -35,13 +35,14 @@ class plgK2Disqus_K2 extends K2Plugin {
 		$parsedInModule = $params->get('parsedInModule');
 
 		if ($parsedInModule == 1){
-			if ($this->isK2()): 
+			if ($this->isK2()):
 			$output = '';
-			else: 
+			else:
 			$output = '
 				<script  type="text/javascript">
-				var disqus_shortname = \''.$shortname.'\';
-				var disqus_url = \''.$site_root.'\';
+				var disqus_shortname = \''.$shortname.'\',
+					disqus_title = \''.$this->getTitle().'\';
+					disqus_url = \''.$site_root.'\';
 				(function () {
 					var s = document.createElement(\'script\'); s.async = true;
 					s.type = \'text/javascript\';
@@ -54,15 +55,16 @@ class plgK2Disqus_K2 extends K2Plugin {
 
 			return $output;
 
-		} 
-		
+		}
+
 		elseif ((!$parsedInModule == 1) && ($this->isArticlePage())) {
-		
+
 			$output = '
 				<script  type="text/javascript">
-				var disqus_shortname = \''.$shortname.'\';
-				var disqus_identifier = \''.$identifier.'\';
-				var disqus_url = \''.$site_root.'\';
+				var disqus_shortname = \''.$shortname.'\',
+					disqus_identifier = \''.$identifier.'\',
+					disqus_title = \''.$this->getTitle().'\',
+					disqus_url = \''.$site_root.'\';
 				(function () {
 					var s = document.createElement(\'script\'); s.async = true;
 					s.type = \'text/javascript\';
@@ -71,13 +73,13 @@ class plgK2Disqus_K2 extends K2Plugin {
 					}());
 				</script>
 				';
-				
+
 				$document = JFactory::getDocument();
 				$doctype  = $document->getType();
-				
+
 				// Only render for HTML output
 				if ( $doctype == 'html' ) {
-					$document->addCustomTag($output); 
+					$document->addCustomTag($output);
 				}
 			}
 
@@ -91,14 +93,15 @@ class plgK2Disqus_K2 extends K2Plugin {
 
 		$site_root = JURI::current();
 		$shortname = $pluginParams->get('shortname');
-		
+
 		if ($this->isArticlePage()){
 			$output = '';
 			} else {
 			$output = '
 				<script  type="text/javascript">
-				var disqus_shortname = \''.$pluginParams->get('shortname').'\';
-				var disqus_url = \''.$site_root.'\';
+				var disqus_shortname = \''.$pluginParams->get('shortname').'\',
+					disqus_title = \''.$this->getTitle().'\',
+					disqus_url = \''.$site_root.'\';
 				(function () {
 					var s = document.createElement(\'script\'); s.async = true;
 					s.type = \'text/javascript\';
@@ -111,10 +114,10 @@ class plgK2Disqus_K2 extends K2Plugin {
 
 			$document = JFactory::getDocument();
 			$doctype  = $document->getType();
-			
+
 			// Only render for HTML output
 			if ( $doctype == 'html' ) {
-				$document->addCustomTag($output); 
+				$document->addCustomTag($output);
 			}
 	}
 
@@ -136,14 +139,14 @@ class plgK2Disqus_K2 extends K2Plugin {
 
 	function onK2CommentsBlock( &$item, &$params, $limitstart) {
 		$mainframe = &JFactory::getApplication();
-		
+
 		$plugin = &JPluginHelper::getPlugin('k2', $this->pluginName);
 		$pluginParams = new JParameter($plugin->params);
 
 		$item_link = $item->link;
 		$identifier = $item->id;
 
-		if ($this->isArticlePage()): 
+		if ($this->isArticlePage()):
 		$output = '<a name="itemCommentsAnchor" id="itemCommentsAnchor"></a>
 
 					<div id="disqus_thread" class="itemComments"></div>
@@ -157,32 +160,32 @@ class plgK2Disqus_K2 extends K2Plugin {
 					</script>
 					
 					';
-		else: 
+		else:
 		$output = '';
 		endif;
 		return $output;
-		
+
 	}
-	
+
 	function isArticlePage()
 	{
 		$option 	 = JRequest::getVar('option');
 		$view 		 = JRequest::getVar('view');
 		if  ($option == 'com_k2' && $view == 'item'/*K2 Specific*/) {
 			return true;
-		} 
+		}
 		return false;
 	}
-	
+
 		function isK2()
 	{
 		$option 	 = JRequest::getVar('option');
 		if  ($option == 'com_k2') {
 			return true;
-		} 
+		}
 		return false;
 	}
 
 
 
-} 
+}
