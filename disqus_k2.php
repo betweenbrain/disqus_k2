7da81448852cd687fbd9b2597e3b84c8e60a50a0
Matt Thomas <matt@betweenbrain.com>
2013-10-01 13:14:27 -0400

    More OOP FTW Removes redundant code Code cleanup

--- disqus_k2.php
+++ disqus_k2.php
@@ -21,19 +21,18 @@ class plgK2Disqus_K2 extends K2Plugin {
 		parent::__construct($subject, $params);
 		jimport('joomla.html.parameter');
 
+		$this->app     = JFactory::getApplication();
 		$this->doc     = JFactory::getDocument();
 		$this->doctype = $this->doc->getType();
-		$this->plugin  = & JPluginHelper::getPlugin('k2', 'disqus_k2');
+		$this->plugin  = JPluginHelper::getPlugin('k2', $this->pluginName);
 		$this->params  = new JParameter($this->plugin->params);
 	}
 
 	function onK2BeforeDisplay(&$item, &$params, $limitstart) {
-		$plugin       = & JPluginHelper::getPlugin('k2', $this->pluginName);
-		$pluginParams = new JParameter($plugin->params);
 
-		$disqusUrl      = $pluginParams->get('disqusUrl');
+		$disqusUrl      = $this->params->get('disqusUrl');
 		$site_root      = JURI::current();
-		$shortname      = $pluginParams->get('shortname');
+		$shortname      = $this->params->get('shortname');
 		$identifier     = $item->id;
 		$parsedInModule = $params->get('parsedInModule');
 
@@ -50,7 +49,7 @@ class plgK2Disqus_K2 extends K2Plugin {
 				(function () {
 					var s = document.createElement(\'script\'); s.async = true;
 					s.type = \'text/javascript\';
-					s.src = \'http://' . $pluginParams->get('shortname') . '.disqus.com/count.js\';
+					s.src = \'http://' . $this->params->get('shortname') . '.disqus.com/count.js\';
 					(document.getElementsByTagName(\'HEAD\')[0] || document.getElementsByTagName(\'BODY\')[0]).appendChild(s);
 					}());
 				</script>
@@ -83,37 +82,23 @@ class plgK2Disqus_K2 extends K2Plugin {
 	}
 
 	function onK2CommentsCounter(&$item, &$params, $limitstart) {
-		$mainframe = & JFactory::getApplication();
 
-		$plugin       = & JPluginHelper::getPlugin('k2', $this->pluginName);
-		$pluginParams = new JParameter($plugin->params);
-
-		$site_root  = $mainframe->getCfg('live_site');
-		$item_link  = $item->link;
-		$identifier = $item->id;
-
-		$output = '<a href= "' . $site_root . $item_link . '#disqus_thread" data-disqus-identifier="' . $identifier . '">' . JText::_('DISQUS_COMMENTS') . '</a>
-		';
+		$output = '<a href= "' . JURI::base() . $item->link . '#disqus_thread" data-disqus-identifier="' . $item->id . '">' . JText::_('DISQUS_COMMENTS') . '</a>';
 
 		return $output;
 	}
 
 	function onK2CommentsBlock(&$item, &$params, $limitstart) {
-		$plugin       = & JPluginHelper::getPlugin('k2', $this->pluginName);
-		$pluginParams = new JParameter($plugin->params);
-
-		$item_link  = $item->link;
-		$identifier = $item->id;
 
 		if ($this->isArticlePage()):
 			$output = '<a name="itemCommentsAnchor" id="itemCommentsAnchor"></a>
 
 					<div id="disqus_thread" class="itemComments"></div>
 					<script type="text/javascript">
-					var disqus_identifier = \'' . $identifier . '\';
+					var disqus_identifier = \'' . $item->id . '\';
 						(function() {
 							var dsq = document.createElement(\'script\'); dsq.type = \'text/javascript\'; dsq.async = true;
-							dsq.src = \'http://' . $pluginParams->get('shortname') . '.disqus.com/embed.js\';
+							dsq.src = \'http://' . $this->params->get('shortname') . '.disqus.com/embed.js\';
 							(document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(dsq);
 						})();
 					</script>
@@ -128,7 +113,7 @@ class plgK2Disqus_K2 extends K2Plugin {
 	function isArticlePage() {
 		$option = JRequest::getVar('option');
 		$view   = JRequest::getVar('view');
-		if ($option == 'com_k2' && $view == 'item' /*K2 Specific*/) {
+		if ($option == 'com_k2' && $view == 'item') {
 			return TRUE;
 		}
 
